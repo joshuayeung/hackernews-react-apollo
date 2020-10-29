9f3a1debf5d7f04ebf0ee7c1bd0061a1a2df3094
Joshua Yeung <joshua.chyeung@gmail.com>
2020-10-29 17:02:43 +0800

    ADD: sending queries to the server

---
+++ src/components/LinkList.js
@@ -0,0 +1,58 @@
+import React from "react";
+import gql from "graphql-tag";
+import "./Link";
+import Link from "./Link";
+import { Query } from "react-apollo";
+
+const LinkList = () => {
+  const FEED_QUERY = gql`
+    {
+      feed {
+        links {
+          id
+          createdAt
+          url
+          description
+        }
+      }
+    }
+  `;
+
+  //   const linksToRender = [
+  //     {
+  //       id: "1",
+  //       description: "Prisma turns your database into a GraphQL API 😎",
+  //       url: "https://www.prismagraphql.com",
+  //     },
+  //     {
+  //       id: "2",
+  //       description: "The best GraphQL client",
+  //       url: "https://www.apollographql.com/docs/react/",
+  //     },
+  //   ];
+
+  return (
+    //   {linksToRender.map((link) => (
+    //     <Link key={link.id} link={link} />
+    //   ))}
+
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) return <div>Error</div>;
+
+        const linksToRender = data.feed.links;
+
+        return (
+          <div>
+            {linksToRender.map((link) => (
+              <Link key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default LinkList;
